3dbab6a4cd786ac98c1899c84b40d01509224f54
Andrew Chellis <andrewchellis@users.noreply.github.com>
2017-05-02 17:52:34 -0400

    Non pruned tree working, but undocumented

--- instrumentNode.py
+++ instrumentNode.py
@@ -1,16 +1,9 @@
-class treeNode:
-    def __init__(self,fingerings,index,value,cost):
+class TreeNode:
+    def __init__(self, fingerings, index, value, cost):
         self.fingerings = fingerings
         self.noteIndex = index
         self.value = value
         self.cost = cost
-    def getIndex(self):
-        return self.noteIndex
-    def getFingerings(self):
-        return self.fingerings
-    def getValue(self):
-        return self.value
-    def getCost(self):
-        return self.cost
     def __str__(self):
-        return "past fingerings: {}, cost: {}. current eval: {}, current index: {}".format(self.fingerings,self.cost,self.value,self.noteIndex)
+        return "past fingerings: {}, cost: {}. current eval: {}, \
+         current index: {}".format(self.fingerings, self.cost, self.value, self.noteIndex)
--- nonprunedTree.py
+++ nonprunedTree.py
@@ -1,41 +1,48 @@
 import sys
 import queue
+from timeit import default_timer as timer
 from instrumentNode import treeNode
-
-
-def main():
-    with open(sys.argv[1], 'r') as f:
-        contents = f.read()
-    contents = contents.split()
-    numNotes = contents[0]
-    
-    process(numNotes,contents[1:])
+from optimal_fingerings import N2F, F2N, DIST
 
 
 def process(numNotes, notes):
     items = queue.Queue()
     finalLevel = []
     count = 0
-    for comb in n2f[int(notes[0])]:
-        ins = trumpetNode([],1,comb,0)
-        ins.getFingerings().append(comb)
+    for comb in N2F[notes[0]]:
+        ins = treeNode([], 1, comb, 0)
+        ins.fingerings.append(comb)
         items.put(ins)
     while not items.empty():
         curr = items.get()
-        count+=1
-        if(curr.getIndex()<len(notes)-1):
+        count += 1
+        if curr.noteIndex < len(notes)-1:
            # print("eval: {}, note:{}".format(curr, curr.getIndex()))
-            for comb in n2f[int(notes[curr.getIndex()])]:
-                ins = trumpetNode(curr.getFingerings()[:],curr.getIndex()+1,comb,curr.getCost()+dist[comb][curr.getValue()])
-                ins.getFingerings().append(comb)
+            for comb in N2F[notes[curr.noteIndex]]:
+                ins = treeNode(curr.fingerings[:], curr.noteIndex+1, comb, \
+                curr.cost+DIST[(comb,curr.value)])
+                ins.fingerings.append(comb)
                 items.put(ins)
         else:
             #print("else eval: {}, note:{}".format(curr, curr.getIndex()))
-            for comb in n2f[int(notes[curr.getIndex()])]:
-                ins = trumpetNode(curr.getFingerings()[:],500,comb,curr.getCost()+dist[comb][curr.getValue()])
-                ins.getFingerings().append(comb)
+            for comb in N2F[notes[curr.noteIndex]]:
+                ins = treeNode(curr.fingerings[:], 500, comb,\
+                curr.cost+DIST[(comb,curr.value)])
+                ins.fingerings.append(comb)
                 finalLevel.append(ins)
-    print(min(finalLevel,key=lambda x: x.getCost()))
-    print("processed:{}, lowest level: {}, total nodes: {}".format(count,len(finalLevel),count+len(finalLevel)))
+    return min(finalLevel, key=lambda x: x.getCost())
+    #print("processed:{}, lowest level: {}, total nodes: {}".format(count,len(finalLevel),count+len(finalLevel)))
 
-main()
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        contents = f.read()
+    contents = contents.split()
+    numNotes = contents[0]
+    for i in range(1,len(contents)):
+        contents[i] = int(contents[i])
+    #print(timeit.timeit('process(numNotes,contents[1:])',number=10))
+    start = timer()
+    bestNode = process(int(contents[0]),contents[1:])
+    end = timer()
+    print(end-start)
+    print(bestNode)
