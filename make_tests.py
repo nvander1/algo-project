b245338a85cabee487081fba51c731c5dfd6b5ed
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-05-03 19:11:12 -0400

    Use random.chocies for test generation

--- make_tests.py
+++ make_tests.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     import random
     from optimal_fingerings import N2F
 
-    ALL_NUMS = N2F.keys()
+    ALL_NUMS = list(N2F.keys())
     SIZE = int(sys.argv[1])
     with open(f'test_files/test_{SIZE}.txt', 'w') as file:
-        file.write('\n'.join(str(n) for n in random.sample(ALL_NUMS, SIZE)))
+        file.write('\n'.join(str(n) for n in random.choices(ALL_NUMS, k=SIZE)))
