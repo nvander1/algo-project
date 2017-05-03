ca1d11f4fe2a99094ea68c42624c2acd434d8d39
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-05-03 19:48:05 -0400

    Make it easy to generate lots of files

--- make_tests.py
+++ make_tests.py
@@ -1,13 +1,27 @@
 """
 A utility for generating test files.
 """
+import sys
+import random
+
+from optimal_fingerings import N2F
+
+ALL_NUMS = list(N2F.keys())
+
+def single_test(size):
+    """
+    Creates a single test file of specified length.
+    """
+    with open(f'test_files/test_{size}.txt', 'w') as file:
+        file.write('\n'.join(str(n) for n in random.choices(ALL_NUMS, k=size)))
 
 if __name__ == '__main__':
-    import sys
-    import random
-    from optimal_fingerings import N2F
-
-    ALL_NUMS = list(N2F.keys())
-    SIZE = int(sys.argv[1])
-    with open(f'test_files/test_{SIZE}.txt', 'w') as file:
-        file.write('\n'.join(str(n) for n in random.choices(ALL_NUMS, k=SIZE)))
+    ARGC = len(sys.argv)
+    if ARGC == 2:
+        single_test(int(sys.argv[1]))
+    elif ARGC == 4:
+        START = int(sys.argv[2])
+        SCALE = int(sys.argv[3])
+        for i in range(1, 1+int(sys.argv[1])):
+            single_test(SCALE * START ** i)
+            print(SCALE * START ** i)
