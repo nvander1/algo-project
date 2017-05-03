f4f30ffad2f8ca57909cf8ec4ad58052aa7a704a
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-05-03 00:27:49 -0400

    Fix pylint errors

--- optimal_fingerings.py
+++ optimal_fingerings.py
@@ -1,18 +1,14 @@
-'''
+"""
 This module contains common utilities
 and constants used in different implementations
 of the optimal fingering problem.
-'''
+"""
 
 
 from collections import defaultdict as dd
 from itertools import product
 
 
-'''
-A mapping from fingerings to a list of notes
-available at each fingering.
-'''
 F2N = {
     '000': [46, 53, 58, 62, 65, 68, 70, 72, 74],
     '010': [45, 52, 57, 61, 64, 67, 69, 71, 73],
@@ -23,27 +19,31 @@ F2N = {
     '101': [41, 48, 53, 57, 60, 63, 65, 67, 69],
     '111': [40, 47, 52, 56, 59, 62, 64, 66, 68]
     }
+"""
+A mapping from fingerings to a list of notes
+available at each fingering.
+"""
 
 
-'''
+N2F = dd(set)
+"""
 A mapping from a note to all possible
 fingerings for a note.
-'''
-N2F = dd(set)
+"""
 for key, val in F2N.items():
     for item in val:
         N2F[item].add(key)
 
 
 def dist(f_1, f_2):
-    '''
+    """
     Calculates the number of moved fingers between
     two fingerings.
-    '''
+    """
     return sum(x != y for x, y in zip(f_1, f_2))
 
 
-'''
-A mapping from pairs of fingerings to the distance between them.
-'''
 DIST = dict((pair, dist(*pair)) for pair in product(F2N, repeat=2))
+"""
+A mapping from pairs of fingerings to the distance between them.
+"""
