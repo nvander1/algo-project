eaa7e16725c7ec8840e2bab28d75c4d0bae88130
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-05-09 03:56:53 -0400

    Add docstring

--- util.py
+++ util.py
@@ -6,6 +6,17 @@ to the optimal fingering problem.
 def read_notes(filename):
     """
     Creates a list of notes values from a newline separated file.
+
+    Parameters
+    ----------
+    filename : string
+        The input file.
+
+    Returns
+    -------
+    notes : (list of int) or None
+        A list of integers representing notes from the input file if
+        all the notes are within range, else None.
     """
     with open(filename, 'r') as test_file:
         notes = [int(line) for line in test_file.read().splitlines()]
