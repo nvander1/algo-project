3809a44284d55010db47468f11c022baf873376a
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-05-09 04:20:40 -0400

    Move readnotes into test.py

--- test.py
+++ test.py
@@ -15,7 +15,6 @@ import plotly as py
 import plotly.graph_objs as go
 
 import optimal_fingerings as of
-import util
 
 
 def make_parser():
@@ -60,6 +59,57 @@ def get_files(directory):
     return sorted(glob.glob(f'{directory}/*'), key=os.path.getsize)
 
 
+def read_notes(filename):
+    """
+    Creates a list of notes values from a newline separated file.
+
+    Parameters
+    ----------
+    filename : string
+        The input file.
+
+    Returns
+    -------
+    notes : (list of int) or None
+        A list of integers representing notes from the input file if
+        all the notes are within range, else None.
+    """
+    with open(filename, 'r') as test_file:
+        notes = [int(line) for line in test_file.read().splitlines()]
+        return notes if all(39 < note < 75 for note in notes) else None
+
+
+def time(files, output, times=1):
+    """
+    Times Bottom-Up and Pruning methods on test files and saves as a csv.
+
+    Parameters
+    ----------
+    files : list of strings
+        A list of files to test.
+    output : string
+        The path at which to save the results.
+    times : int
+        How many times to execute each test.
+    """
+    with open(output, 'w') as csv_file:
+        fields = ['Input Size', 'Bottom-Up', 'Pruning']
+        writer = csv.DictWriter(csv_file, fieldnames=fields)
+        writer.writeheader()
+
+        average = lambda code: timeit(code, number=times)/times
+
+        for filename in files:
+            notes = read_notes(filename)
+            size = len(notes)
+            written = {'Input Size': size}
+            written['Bottom-Up'] = average(lambda: of.bottom_up(notes))
+            written['Pruning'] = average(lambda: of.pruning(notes))
+            writer.writerow(written)
+            print(f'Finished testing {filename}')
+        print(f'Finished testing {len(files)} files')
+
+
 def graph(results):
     """
     Graphs result data with plotly.
@@ -88,37 +138,6 @@ def graph(results):
     fig = go.Figure(data=[bottom_up, pruning], layout=layout)
 
     py.offline.plot(fig, filename='Optimal_Fingering_Algorithms')
-
-
-def time(files, output, times=1):
-    """
-    Times Bottom-Up and Pruning methods on test files and saves as a csv.
-
-    Parameters
-    ----------
-    files : list of strings
-        A list of files to test.
-    output : string
-        The path at which to save the results.
-    times : int
-        How many times to execute each test.
-    """
-    with open(output, 'w') as csv_file:
-        fields = ['Input Size', 'Bottom-Up', 'Pruning']
-        writer = csv.DictWriter(csv_file, fieldnames=fields)
-        writer.writeheader()
-
-        average = lambda code: timeit(code, number=times)/times
-
-        for filename in files:
-            notes = util.read_notes(filename)
-            size = len(notes)
-            written = {'Input Size': size}
-            written['Bottom-Up'] = average(lambda: of.bottom_up(notes))
-            written['Pruning'] = average(lambda: of.pruning(notes))
-            writer.writerow(written)
-            print(f'Finished testing {filename}')
-        print(f'Finished testing {len(files)} files')
 
 
 if __name__ == '__main__':
